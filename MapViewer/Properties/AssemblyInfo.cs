e61a96d2783fec2766cf048c866798f71896ba69
Anders Blom <anders.blom@ilexis.se>
2020-04-05 15:48:19 +0200

    New version 1.19

--- MapViewer/Properties/AssemblyInfo.cs
+++ MapViewer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Miakonn")]
 [assembly: AssemblyProduct("MapViewer")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -42,7 +42,7 @@ using System.Windows;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
+//      Major Version 
 //      Minor Version 
 //      Build Number
 //      Revision
@@ -50,5 +50,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.18.0.0")]
-[assembly: AssemblyFileVersion("1.18.0.0")]
+[assembly: AssemblyVersion("1.19.0.0")]
+[assembly: AssemblyFileVersion("1.19.0.0")]
