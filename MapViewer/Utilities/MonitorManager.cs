37cd5cb8e298125c5c3a54a3a09245bbe9ccd4db
Anders Blom <anders.blom@ilexis.se>
2020-04-05 15:49:32 +0200

    Just cleaning and updating MonitorManager.

--- MapViewer/Utilities/MonitorManager.cs
+++ MapViewer/Utilities/MonitorManager.cs
@@ -8,214 +8,169 @@ using System.Threading;
 using System.Windows;
 using System.Windows.Input;
 
-
 namespace MapViewer.Utilities {
+    
+    public class MonitorManager {
+        private readonly Edid _edid;
+
+        private static readonly log4net.ILog Log =
+            log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public MonitorManager() {
+            _edid = new Edid(RunDumpEdid());
+        }
+
+        private static string RunDumpEdid() {
+            Mouse.OverrideCursor = Cursors.Wait;
+            try {
+                var path = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
+                if (path == null) {
+                    return null;
+                }
+
+                var process = new Process {
+                    StartInfo = {
+                        FileName = Path.Combine(path, "DumpEDID\\DumpEDID.exe"),
+                        Arguments = "-a",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+                process.Start();
+                Thread.Sleep(1000);
+                Mouse.OverrideCursor = null;
+
+                return process.StandardOutput.ReadToEnd();
+            }
+            catch (Exception ex) {
+                Log.Error("Failed to run DumpEDID.exe.", ex);
+                MessageBox.Show("Failed to run DumpEDID.exe: \n" + ex.Message);
+            }
+
+            Mouse.OverrideCursor = null;
+            return null;
+        }
+
+        public List<string> MonitorList => (from monitor in _edid.Monitors
+            where !monitor.Active.HasValue || monitor.Active.Value
+            select monitor.Name).ToList();
+
+        public List<Monitor> Monitors => _edid.Monitors;
 
 
-	public class MonitorManager {
-		private readonly Edid _edid;
+        public class Monitor {
+            public bool? Active {
+                get {
+                    if (Parameters.ContainsKey("Active")) {
+                        return Parameters["Active"].Contains("Yes");
+                    }
 
-		private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+                    return null;
+                }
+            }
 
-		public MonitorManager() {
-			_edid = new Edid(RunDumpEdid());
-		}
+            /// <summary>
+            /// Return Image Size in mm, fall back to Maximum Images Size
+            /// </summary>
+            public Size? ImageSize => ParseSizeFloat10("Image Size") ?? ParseSizeFloat10("Maximum Image Size");
 
-		private static string RunDumpEdid() {
-			Mouse.OverrideCursor = Cursors.Wait;
-			try {
-				var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-				if (path == null) {
-					return null;
-				}
-				var process = new Process();
-				process.StartInfo.FileName = Path.Combine(path, "DumpEDID\\DumpEDID.exe");
-				process.StartInfo.Arguments = "-a";
-				process.StartInfo.UseShellExecute = false;
-				process.StartInfo.RedirectStandardOutput = true;
-				process.StartInfo.CreateNoWindow = true;
-				process.Start();
-				Thread.Sleep(1000);
-				Mouse.OverrideCursor = null;
+            /// <summary>
+            /// Return Maximum Resolution in pixels
+            /// </summary>
+            public Size? MaximumResolution => ParseSizeInt("Maximum Resolution");
 
-#if DEBUG_OFF
-				return File.ReadAllText(Path.Combine(path, "Mickes-skärmar.txt"));
-#else 
-				return process.StandardOutput.ReadToEnd();
-#endif
-			}
-			catch (Exception ex) {
-				Log.Error("Failed to run DumpEDID.exe.", ex);
-				MessageBox.Show("Failed to run DumpEDID.exe: \n" + ex.Message);
-			}
-			Mouse.OverrideCursor = null;
-			return null;
-		}
+            /// <summary>
+            /// Return some format of name
+            /// </summary>
+            public string Name {
+                get {
+                    if (Parameters.ContainsKey("Monitor Name")) {
+                        return Parameters["Monitor Name"];
+                    }
 
-		public List<string> MonitorList {
-			get {
-				return (from monitor in _edid.Monitors where !monitor.Active.HasValue || monitor.Active.Value select monitor.Name).ToList();
-			}
-		}
+                    if (Parameters.ContainsKey("Registry Key")) {
+                        var param = Parameters["Registry Key"];
+                        var parts = param.Split('\\');
+                        if (parts.Length == 3) {
+                            return parts[1];
+                        }
+                    }
 
-		public List<Monitor> Monitors {
-			get {
-				return _edid.Monitors;
-			}
-		}
+                    if (Parameters.ContainsKey("ManufacturerID")) {
+                        return Parameters["ManufacturerID"];
+                    }
 
-		public Monitor GetLargestActiveMonitor() {
-			if (_edid.Monitors.Count == 0) {
-				return null;
-			}
-			Monitor monitorLargest = null;
-			double valueLargest = -1;
-			foreach (var monitor in _edid.Monitors) {
-				if (monitor.ImageSize.HasValue && monitor.ImageSize.Value.Width > valueLargest) {
-					monitorLargest = monitor;
-					valueLargest = monitor.ImageSize.Value.Width;
-				}
-			}
-			if (monitorLargest == null) {
-				Log.InfoFormat("Found no active monitors!");
-			}
-			else  if (monitorLargest.Parameters.ContainsKey("Serial Number")) {
-				Log.InfoFormat("Selected monitor: " + monitorLargest.Parameters["Serial Number"]);
-			}
-			else if (monitorLargest.Parameters.ContainsKey("Serial Number")) {
-				Log.InfoFormat("Selected monitor: " + monitorLargest.Parameters["Serial Number"]);
-			}
-			else if (monitorLargest.Parameters.ContainsKey("Registry Key")) {
-				Log.InfoFormat("Selected monitor: " + monitorLargest.Parameters["Registry Key"]);
-			}
-			else {
-				Log.InfoFormat("Selected monitor: Unknown");
-			}
-			return monitorLargest;
-		}
-	}
+                    return "Unknown";
+                }
+            }
 
-	public class Monitor {
-		private readonly Dictionary<string, string> _parameters = new Dictionary<string, string>();
+            public Dictionary<string, string> Parameters { get; } = new Dictionary<string, string>();
 
-		public bool? Active {
-			get {
-				if (_parameters.ContainsKey("Active")) {
-					return _parameters["Active"].Contains("Yes");
-				}
-				return null;
-			}
-		}
+            private Size? ParseSizeFloat10(string key) {
+                if (Parameters.ContainsKey(key)) {
+                    var parts = Parameters[key].Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    if (parts.Length >= 3 && double.TryParse(parts[0], out var width) &&
+                        double.TryParse(parts[2], out var height)) {
+                        return new Size((int) (10 * width), (int) (10 * height));
+                    }
+                }
 
-		/// <summary>
-		/// Return Image Size in mm, fall back to Maximum Images Size
-		/// </summary>
-		public Size? ImageSize {
-			get {
-				var size = ParseSizeFloat10("Image Size");
-				if (!size.HasValue) {
-					size = ParseSizeFloat10("Maximum Image Size");
-				}
-				return size;
-			}
-		}
+                return null;
+            }
 
-		/// <summary>
-		/// Return Maximum Resolution in pixels
-		/// </summary>
-		public Size? MaximumResolution {
-			get {
-				return ParseSizeInt("Maximum Resolution");
-			}
-		}
+            private Size? ParseSizeInt(string key) {
+                if (Parameters.ContainsKey(key)) {
+                    var parts = Parameters[key].Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    if (parts.Length >= 3 && int.TryParse(parts[0], out var width) &&
+                        int.TryParse(parts[2], out var height)) {
+                        return new Size(width, height);
+                    }
+                }
 
-		/// <summary>
-		/// Return some format of name
-		/// </summary>
-		public string Name {
-			get {
-				if (_parameters.ContainsKey("Monitor Name")) {
-					return _parameters["Monitor Name"];
-				}
-				if (_parameters.ContainsKey("Registry Key")) {
-					var param = _parameters["Registry Key"];
-					var parts = param.Split('\\');
-					if (parts.Length == 3) {
-						return parts[1];
-					}
-				}
-				if (_parameters.ContainsKey("ManufacturerID")) {
-					return _parameters["ManufacturerID"];
-				}
-				return "Unknown";
-			}
-		}
+                return null;
+            }
 
-		public Dictionary<string, string> Parameters {
-			get { return _parameters; }
-		}
+        }
 
-		private Size? ParseSizeFloat10(string key) {
-			if (_parameters.ContainsKey(key)) {
-				double width, height;
-				var parts = _parameters[key].Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-				if (parts.Length >= 3 && double.TryParse(parts[0], out width) && double.TryParse(parts[2], out height)) {
-					return new Size((int)(10 * width), (int)(10 * height));
-				}
-			}
-			return null;
-		}
+        internal class Edid {
+            private readonly List<Monitor> _monitors = new List<Monitor>();
 
-		private Size? ParseSizeInt(string key) {
-			if (_parameters.ContainsKey(key)) {
-				int width, height;
-				var parts = _parameters[key].Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-				if (parts.Length >= 3 && int.TryParse(parts[0], out width) && int.TryParse(parts[2], out height)) {
-					return new Size(width, height);
-				}
-			}
-			return null;
-		}
+            public List<Monitor> Monitors => _monitors;
 
-	}
+            private static void ParseLine(string line, Monitor monitor) {
+                var parts = line.Split(":".ToCharArray());
+                if (parts.Length == 2) {
+                    monitor.Parameters.Add(parts[0].Trim(), parts[1].Trim());
+                }
+            }
 
-	internal class Edid {
-		private readonly List<Monitor> _monitors = new List<Monitor>();
-		private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            public Edid(string text) {
+                if (string.IsNullOrWhiteSpace(text)) {
+                    return;
+                }
 
-		public List<Monitor> Monitors {
-			get { return _monitors; }
-		}
+                text = text.Replace("\r\r\n", "\r\n");
+                if (string.IsNullOrWhiteSpace(text)) {
+                    return;
+                }
 
-		private static void ParseLine(string line, Monitor monitor) {
-			var parts = line.Split(":".ToCharArray());
-			if (parts.Length == 2) {
-				monitor.Parameters.Add(parts[0].Trim(), parts[1].Trim());
-			}
-		}
+                Log.Info(text);
+                var lines = text.Split("\n\r".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
 
-		public Edid(string text) {
-			if (string.IsNullOrWhiteSpace(text)) {
-				return;
-			}
-			text = text.Replace("\r\r\n", "\r\n");
-			if (string.IsNullOrWhiteSpace(text)) {
-				return;
-			}
-			Log.Info(text);
-			var lines = text.Split("\n\r".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-
-			Monitor monitor = null;
-			foreach (var line in lines) {
-				if (line.StartsWith("Active")) {
-					monitor = new Monitor();
-					_monitors.Add(monitor);
-					ParseLine(line, monitor);
-				}
-				else if (monitor != null) {
-					ParseLine(line, monitor);
-				}
-			}
-		}
-	}
+                Monitor monitor = null;
+                foreach (var line in lines) {
+                    if (line.StartsWith("Active")) {
+                        monitor = new Monitor();
+                        _monitors.Add(monitor);
+                        ParseLine(line, monitor);
+                    }
+                    else if (monitor != null) {
+                        ParseLine(line, monitor);
+                    }
+                }
+            }
+        }
+    }
 }
+
